ff5d583f26eecdc755483b56a02ac3ee50a92cfb
R0Y15 <bishalroy15jan@gmail.com>
2023-03-09 23:35:09 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let text = document.querySelector('.banner h2');
+
+window.addEventListener('scroll', () => {
+  let value = window.scrollY;
+  text.style.marginBottom = value * 2 + 'px';
+})
+
+$(document).ready(function () {
+
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function () {
+
+    /* Check the location of each desired element */
+    $('.hideme').each(function (i) {
+
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > bottom_of_object) {
+
+        $(this).animate({ 'opacity': '1' }, 1500);
+
+      }
+
+    });
+
+  });
+
+});
