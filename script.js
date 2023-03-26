2b9c7cfc1d8d30c068b685f702071a24aea078d3
R0Y15 <bishalroy15jan@gmail.com>
2023-03-26 16:59:56 +0530

    new updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 // navbar
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {scrollFunction() };
 function scrollFunction() {
   var navbar = document.getElementById("navbar");
   var footer = document.getElementById("footer");
@@ -20,22 +20,19 @@ function scrollFunction() {
   }
 }
 
-
+//body
 $(document).ready(function () {
   /* Every time the window is scrolled ... */
   $(window).scroll(function () {
     /* Check the location of each desired element */
     $('.hideme').each(function (i) {
-      var bottom_of_object = $(this).position().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
       var top_of_object = $(this).position().top;
       var top_of_window = $(window).scrollTop();
 
-      /* If the top of the object is visible in the window, fade it in */
+      /* If the top of the object is visible in the window, add class to show it */
       if (top_of_window + ($(window).height() / 2) > top_of_object) {
-        $(this).animate({ 'opacity': '1' }, 1500 * (i + 1));
+        $(this).addClass('appear').css('transition-delay', i * 0.5 + 's');
       }
     });
   });
 });
-
