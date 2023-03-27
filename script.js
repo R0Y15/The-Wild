2d45816f817b24bbde9264a235b9320d6fd5bc65
R0Y15 <bishalroy15jan@gmail.com>
2023-03-27 20:24:31 +0530

    new moveIn

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
 // tootltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-// navbar
-// Scroll function
+// navbar function
 function scrollFunction() {
   var navbar = document.getElementById("navbar");
   var footer = document.getElementById("footer");
@@ -20,45 +18,27 @@ function scrollFunction() {
   }
 }
 
-// Highlight active tab function
-function highlightActiveTab() {
-  // Get the current page URL
-  var currentUrl = window.location.href;
+// scroll function
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
 
-  // Find the links in the navbar
-  var navLinks = document.querySelectorAll('#navbar a');
+  for (var i = 0; i < reveals.length; i++) {
 
-  // Loop through the links and check if their href matches the current URL
-  for (var i = 0; i < navLinks.length; i++) {
-    if (navLinks[i].href === currentUrl) {
-      // Add the "active" class to the link if it matches the current URL
-      navLinks[i].classList.add('active');
-    } else {
-      // Remove the "active" class from the link if it doesn't match the current URL
-      navLinks[i].classList.remove('active');
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 250;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('revealup');
+    }
+    else {
+      reveals[i].classList.remove('revealup');
     }
   }
 }
 
-// Scrollspy function
-$(document).ready(function () {
-  /* Every time the window is scrolled ... */
-  $(window).scroll(function () {
-    /* Check the location of each desired element */
-    $('.hideme').each(function (i) {
-      var top_of_object = $(this).position().top;
-      var top_of_window = $(window).scrollTop();
-
-      /* If the top of the object is visible in the window, add class to show it */
-      if (top_of_window + ($(window).height() / 2) > top_of_object) {
-        $(this).addClass('appear').css('transition-delay', i * 0.5 + 's');
-      }
-    });
-  });
-});
-
 // Attach scroll and highlightActiveTab functions to window.onscroll
-window.onscroll = function() {
+window.onscroll = function () {
   scrollFunction();
-  highlightActiveTab();
+  reveal();
 };
